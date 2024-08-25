09fbf22ab0bb2bb2e1d2c77402775c9f2e120887
Uknownsa <elvis.artesantao@gmail.com>
2024-08-25 14:47:28 -0300

    commita

--- projetoDomingo/src/App.tsx
+++ projetoDomingo/src/App.tsx
@@ -1,35 +1,263 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Autocomplete,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import "./App.css";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import pipi from "./assets/WhatsApp Image 2024-08-17 at 19.54.32.jpeg";
+import boy from "./assets/clip-excited-person-gif-10.gif";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isSecondDialogOpen, setIsSecondDialogOpen] = useState(false);
+  const [isThirdDialogOpen, setIsThirdDialogOpen] = useState(false);
+  const [isFourthDialogOpen, setIsFourthDialogOpen] = useState(false);
+  const [isFivethDialogOpen, setIsFivethDialogOpen] = useState(false);
+  const [isSixthDialogOpen, setIsSixthDialogOpen] = useState(false);
+
+  const [isNome, setIsNome] = useState(null);
+
+  const [selectedCatName, setSelectedCatName] = useState(null);
+  const [selectedCat2Name, setSelectedCat2Name] = useState(null);
+
+  const handleClick = () => {
+    setIsDialogOpen(false);
+    setIsSecondDialogOpen(true);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(selectedCatName);
+    if (selectedCatName && selectedCatName.id == 1) {
+      setIsSecondDialogOpen(false);
+      setIsThirdDialogOpen(true);
+    } else {
+      toast.error("Errou");
+    }
+  };
+  const handleSubmit2 = (e: any) => {
+    e.preventDefault();
+    console.log(selectedCat2Name);
+    if (selectedCat2Name && selectedCat2Name.id == 4) {
+      toast.success("bingo!");
+      setIsThirdDialogOpen(false);
+      setIsFourthDialogOpen(true);
+    } else {
+      toast.error("Errou");
+    }
+  };
+  const handleSubmit3 = (e: any) => {
+    e.preventDefault();
+    if (isNome == "Yasmin") {
+      toast.success("convencida");
+      setIsFourthDialogOpen(false);
+
+      setIsFivethDialogOpen(true);
+    } else {
+      toast.error("errou");
+    }
+  };
+
+  const [buttonPosition, setButtonPosition] = useState({
+    top: "50%",
+    left: "50%",
+  });
+
+  const handleNoClick = () => {
+    // Generate random coordinates within the bounds of the dialog content
+    const randomTop = Math.random() * 80 + "%"; // Between 0% and 80%
+    const randomLeft = Math.random() * 80 + "%"; // Between 0% and 80%
+
+    // Update button position
+    setButtonPosition({ top: randomTop, left: randomLeft });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Olá Yasmin bem-vinda!</h1>
+      <p>Clique no botão para continuar!</p>
+      <Button
+        onClick={() => setIsDialogOpen(true)}
+        sx={{ backgroundColor: "green ", color: "white" }}
+      >
+        Oi eu sou o botão
+      </Button>
+      <Dialog
+        maxWidth="sm"
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+      >
+        <DialogContent>Você está pronta para um rápido quiz?</DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => toast.error("Para de ser chata Yasmin")}
+            sx={{ backgroundColor: "green ", color: "white" }}
+          >
+            Não, eu sou chata bruno
+          </Button>
+          <Button
+            onClick={() => handleClick()}
+            sx={{ backgroundColor: "green ", color: "white" }}
+          >
+            Sim, estou bruno
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={isSecondDialogOpen} maxWidth="md">
+        <DialogTitle>Pergunta 1</DialogTitle>
+        <DialogContent>
+          <p>Como é o nome da minha gata?</p>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={[
+                { id: 1, label: "Angelina" },
+                { id: 2, label: "Angeline" },
+                { id: 3, label: "Ange" },
+                { id: 4, label: "Aline" },
+                { id: 5, label: "Robson" },
+              ]}
+              sx={{ width: 300, height: 200 }}
+              onChange={(event, newValue) => setSelectedCatName(newValue)}
+              renderInput={(params) => <TextField {...params} label="Gato" />}
+            />
+            <DialogActions>
+              <Button
+                type="submit"
+                sx={{ backgroundColor: "green ", color: "white" }}
+              >
+                Confirmar resposta
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isThirdDialogOpen} maxWidth="md">
+        <DialogTitle>Pergunta 2</DialogTitle>
+        <DialogContent>
+          <p>Como é o nome desse gato?</p>
+          <br />
+          <form onSubmit={handleSubmit2}>
+            <img
+              style={{
+                marginLeft: 80,
+              }}
+              src={pipi}
+              alt="pipi"
+              height={200}
+            />
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={[
+                { id: 1, label: "Penélope" },
+                { id: 2, label: "Popo" },
+                { id: 3, label: "Pípi" },
+                { id: 4, label: "Pipi" },
+                { id: 5, label: "Pitribi" },
+              ]}
+              sx={{ width: 300, height: 200 }}
+              onChange={(event, newValue) => setSelectedCat2Name(newValue)}
+              renderInput={(params) => <TextField {...params} label="Gato" />}
+            />
+            <DialogActions>
+              <Button
+                type="submit"
+                sx={{ backgroundColor: "green ", color: "white" }}
+              >
+                Confirmar resposta
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isFourthDialogOpen} maxWidth="md">
+        <DialogTitle>Pergunta 3</DialogTitle>
+        <DialogContent>
+          <p>Digite o nome de uma mulher bonita</p>
+          <br />
+          <p>Digite:</p>
+          <br />
+
+          <form onSubmit={handleSubmit3}>
+            <TextField
+              label="Nome"
+              value={isNome}
+              onChange={(e) => {
+                setIsNome(e.target.value);
+              }}
+            />
+            <DialogActions>
+              <Button
+                type="submit"
+                sx={{ backgroundColor: "green ", color: "white" }}
+              >
+                Confirmar resposta
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isFivethDialogOpen} maxWidth="md">
+        <DialogTitle>Parabéns!!</DialogTitle>
+        <DialogContent>
+          <p>Você completou o quiz!</p>
+          <br />
+          <p>Pergunta Final</p>
+          <br />
+          <p>Topa sair sexta?</p>
+
+          <DialogActions>
+            <Button
+              type="button"
+              onClick={handleNoClick}
+              sx={{
+                backgroundColor: "red",
+                color: "white",
+                position: "absolute",
+                ...buttonPosition,
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              Não
+            </Button>
+            <Button
+              type="submit"
+              onClick={() => {
+                setIsFivethDialogOpen(false);
+                setIsSixthDialogOpen(true);
+              }}
+              sx={{ backgroundColor: "green ", color: "white" }}
+            >
+              Sim
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={isSixthDialogOpen}>
+        <DialogTitle>Fechou! Te vejo lá gatinha!!!</DialogTitle>
+        <DialogContent>
+          {" "}
+          <img
+            style={{
+              marginLeft: 20,
+            }}
+            src={boy}
+            alt="happy"
+            height={200}
+          />
+        </DialogContent>
+      </Dialog>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
